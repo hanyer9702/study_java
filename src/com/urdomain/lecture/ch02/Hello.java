878dcc8c1139d6042cf34d6ec7bf6e21fd094d40
owner <hanyer9702@naver.com>
2022-02-03 17:40:20 +0900

    220203

--- src/com/urdomain/lecture/ch02/Hello.java
+++ src/com/urdomain/lecture/ch02/Hello.java
@@ -3,7 +3,18 @@ package com.urdomain.lecture.ch02;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("wow hello world");
+		System.out.println("너무나도 재밋는 자바 교실");
+		
+		int a; //선언
+		a = 1; //대입
+		
+		int b = 2;
+		
+		int c = 0;
+		
+		c = a + b;
+		
+		System.out.println(c);
 	}
 
 }
