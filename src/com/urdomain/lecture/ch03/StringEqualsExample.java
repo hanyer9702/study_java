4c42d5e3fbb361c63c1fed03aa76c94f85db3a61
owner <hanyer9702@naver.com>
2022-02-10 15:49:21 +0900

    220210

--- src/com/urdomain/lecture/ch03/StringEqualsExample.java
+++ src/com/urdomain/lecture/ch03/StringEqualsExample.java
@@ -4,7 +4,19 @@ public class StringEqualsExample {
 
 	public static void main(String[] args) {
 		
-
+		String str1 = "한예린";
+		String str2 = "한예린";
+		String str3 = new String("한예린");
+		
+		boolean result1 = (str1 == str2);
+		System.out.println("result1= " + result1);
+		
+		boolean result2 = (str1 == str3);
+		System.out.println("result2= " + result2);
+	
+		System.out.println("str1.equals(str2): " + str1.equals(str2)); // == (x) true
+		System.out.println("str1.equals(str3): " + str1.equals(str3)); // == (x) true
+		
 	}
 
 }
