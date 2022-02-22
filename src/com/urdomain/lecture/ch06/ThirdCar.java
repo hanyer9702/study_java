7b0e65d6e1b4897d21e3e65b35f8fc5230136818
owner <hanyer9702@naver.com>
2022-02-22 16:32:41 +0900

    220222

---
+++ src/com/urdomain/lecture/ch06/ThirdCar.java
@@ -0,0 +1,32 @@
+package com.urdomain.lecture.ch06;
+
+public class ThirdCar {
+
+	//필드
+	String company = "현대자동차";
+	String model;
+	String color;
+	int maxSpeed;
+	
+	//생성자
+	ThirdCar(){
+		
+	}
+	
+	ThirdCar(String model){
+		this(model, null, 0);
+	}
+	
+	ThirdCar(String model, String color){
+		this(model, color, 0);
+	}
+	
+	ThirdCar(String model, String color, int maxSpeed){
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
+	}
+	
+	
+
+}
