c811fd83bf9c81d725830cde0a222412bedbc31d
owner <hanyer9702@naver.com>
2022-02-23 17:38:00 +0900

    220223

--- src/com/urdomain/lecture/ch06/FinalCar.java
+++ src/com/urdomain/lecture/ch06/FinalCar.java
@@ -10,12 +10,18 @@ public class FinalCar {
 		return speed;
 	}
 	public void setSpeed(int speed) {
-		this.speed = speed;
+		if(speed < 0) {
+			this.speed = 0;
+			return;
+		} else {
+			this.speed = speed;
+		}
 	}
 	public boolean isStop() {
 		return stop;
 	}
 	public void setStop(boolean stop) {
 		this.stop = stop;
+		this.speed = 0;
 	}
 }
