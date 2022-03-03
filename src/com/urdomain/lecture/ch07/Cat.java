ccc25fe48a3ced4c8374eac59ea375184d39dd1b
owner <hanyer9702@naver.com>
2022-03-03 17:15:16 +0900

    220303

---
+++ src/com/urdomain/lecture/ch07/Cat.java
@@ -0,0 +1,15 @@
+package com.urdomain.lecture.ch07;
+
+public class Cat extends Animal{
+	
+	//积己磊
+	public Cat() {
+		this.kind = "器蜡幅";
+	}
+	
+	@Override
+	public void sound() {
+		System.out.println("具克");
+	}
+
+}
