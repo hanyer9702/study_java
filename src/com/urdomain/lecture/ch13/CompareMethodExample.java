7920de6b5bcd3cf1e0da209d760971695f10b1bd
owner <hanyer9702@naver.com>
2022-03-15 17:25:36 +0900

    220315

---
+++ src/com/urdomain/lecture/ch13/CompareMethodExample.java
@@ -0,0 +1,25 @@
+package com.urdomain.lecture.ch13;
+
+public class CompareMethodExample {
+
+	public static void main(String[] args) {
+		Pair<Integer, String> p1 = new Pair<Integer, String>(1, "사과");
+		Pair<Integer, String> p2 = new Pair<Integer, String>(1, "사과");
+		boolean result1 = Util2.<Integer, String>compare(p1, p2);
+		if(result1) {
+			System.out.println("논리적으로 동등한 객체입니다.");
+		} else {
+			System.out.println("논리적으로 동등하지 않는 객체입니다.");
+		}
+		
+		Pair<String, String> p3 = new Pair<String, String>("user1", "고구마");
+		Pair<String, String> p4 = new Pair<String, String>("user2", "감자");
+		boolean result2 = Util2.compare(p3, p4);
+		if(result2) {
+			System.out.println("논리적으로 동등한 객체입니다.");
+		} else {
+			System.out.println("논리적으로 동등하지 않는 객체입니다.");
+		}
+	}
+
+}
