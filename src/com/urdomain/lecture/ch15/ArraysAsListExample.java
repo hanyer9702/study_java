b973f374125841875cff35b604f9064fcc4a9828
owner <hanyer9702@naver.com>
2022-03-16 18:21:33 +0900

    220316

---
+++ src/com/urdomain/lecture/ch15/ArraysAsListExample.java
@@ -0,0 +1,20 @@
+package com.urdomain.lecture.ch15;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraysAsListExample {
+
+	public static void main(String[] args) {
+		List<String> list1 = Arrays.asList("고구마","감자","토마토");
+		for(String name: list1) {
+			System.out.println(name);
+		}
+		
+		List<Integer> list2 = Arrays.asList(1, 2, 3);
+		for(int value : list2) {
+			System.out.println(value);
+		}
+	}
+
+}
