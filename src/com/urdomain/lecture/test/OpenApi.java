2b6d72b62cc76d30e214b7524b6f92aa7361753b
owner <hanyer9702@naver.com>
2022-10-07 22:23:48 +0900

    키 가리기

--- src/com/urdomain/lecture/test/OpenApi.java
+++ src/com/urdomain/lecture/test/OpenApi.java
@@ -14,7 +14,7 @@ public class OpenApi {
 		// 1. URL을 만들기 위한 StringBuilder.
         StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B552657/HsptlAsembySearchService/getHsptlMdcncFullDown?ServiceKey=OQ%2B71b0Rk3siR26hJdsAYfdC13NcbXfL1SMQ5hOu5NGsvVnov4ynwKxoA1fNVDFKuj6n9KVSKxe1TTYIKReHjw%3D%3D&pageNo=1&numOfRows=10"); /*URL*/
         // 2. 오픈 API의요청 규격에 맞는 파라미터 생성, 발급받은 인증키.
-        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=OQ%2B71b0Rk3siR26hJdsAYfdC13NcbXfL1SMQ5hOu5NGsvVnov4ynwKxoA1fNVDFKuj6n9KVSKxe1TTYIKReHjw%3D%3D"); /*Service Key*/
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "="); /*Service Key*/
         urlBuilder.append("&" + URLEncoder.encode("returnType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*XML 또는 JSON*/
         urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
         urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
